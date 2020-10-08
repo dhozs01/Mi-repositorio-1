2b8a867807ebb8176b0d20748cdfab69aba66d42
diegu <diegu@172.16.5.232>
2020-10-08 18:19:31 +0200

    Linea de texto nueva

--- Documents/NetBeansProjects/MiProyecto/src/miproyecto/MiProyecto.java
+++ Documents/NetBeansProjects/MiProyecto/src/miproyecto/MiProyecto.java
@@ -17,6 +17,7 @@ public class MiProyecto {
     public static void main(String[] args) {
     
         System.out.println("Hola soy Diego");
+        System.out.println("Hola soy Diego");
     }
     
     
